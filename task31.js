b88b2ec2da21793b09e9fdc618e4214578d314b4
JIANGYIMING <jiangyiming@JIANGYIMINGs-MacBook-Pro.local>
2016-04-15 18:02:19 +0800

    finish radios'linkage

--- task31.js
+++ task31.js
@@ -1,3 +1,28 @@
 /**
  * Created by jiangyiming on 4/15/16.
  */
+window.onload=function(){
+    var oInschool = document.getElementById("inschool");
+    var oOutschool = document.getElementById("outschool");
+    addEventHandler(oInschool,"change",modify);
+    addEventHandler(oOutschool,"change",modify);
+};
+function modify(){
+    if (this==document.getElementById("inschool")){
+        document.getElementById("company").style.visibility="hidden"
+        document.getElementById("select").style.visibility="visible"
+    }else {
+        document.getElementById("select").style.visibility="hidden"
+        document.getElementById("company").style.visibility="visible"
+
+    }
+}
+function addEventHandler(ele, event, hanlder) {//浏览器兼容
+    if (ele.addEventListener) {
+        ele.addEventListener(event, hanlder, false);
+    } else if (ele.attachEvent) {
+        ele.attachEvent("on" + event, hanlder);
+    } else {
+        ele["on" + event] = hanlder;
+    }
+}
